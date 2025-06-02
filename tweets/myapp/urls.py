4e4d7931c1dbc3e18850c02a18cd284b421df32b
Isoo Pal <17isoo12@gmail.com>
2025-06-02 14:58:58 +0530

    Register, Login and Log Out

--- tweets/myapp/urls.py
+++ tweets/myapp/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.tweet_list, name = "tweet_list"),
     path('create/', views.tweet_create, name = "tweet_create"),
     path('edit/<int:tweet_id>', views.tweet_edit, name = "tweet_edit"),
-    path('delete/', views.tweet_delete, name = "tweet_delete"),
+    path('delete/<int:tweet_id>', views.tweet_delete, name = "tweet_delete"),
+    path('register/', views.register, name = "register"),
     
 ]
