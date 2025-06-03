70138cef4bf985f96245dcc32c547a926165be5a
Isoo Pal <17isoo12@gmail.com>
2025-06-03 15:24:46 +0530

    Login and Logout fix

--- tweets/myapp/models.py
+++ tweets/myapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 # Create your models here.
 
 class Tweet(models.Model):
@@ -8,6 +9,18 @@ class Tweet(models.Model):
     photo = models.ImageField(upload_to='photos/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)  # Save the image first
+
+    #     img_path = self.image.path
+    #     img = Image.open(img_path)
+
+    #     # Resize to fixed size (example: 300x300)
+    #     output_size = (300, 300)
+    #     img = img.resize(output_size, Image.ANTIALIAS)
+    #     img.save(img_path)
 
 def __str__(self):
     return f'{self.user.username} - {self.text[:10]}'
